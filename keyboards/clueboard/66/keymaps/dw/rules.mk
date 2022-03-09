8176e6c90dbfb2ba1267fb10c38fbffaa22682d4
m5kR10bHxS <dlwhite67@gmail.com>
2022-03-09 17:10:06 -0600

    rules.mk files commonized

--- keyboards/clueboard/66/keymaps/dw/rules.mk
+++ keyboards/clueboard/66/keymaps/dw/rules.mk
@@ -1,34 +1,46 @@
-# Alps64 rules.mk
+# Clueboard 66 rev2 rules.mk
 
-# Build Options
-BACKLIGHT_DRIVER = custom
+# Bootloader selection
+#   Teensy       halfkay
+#   Pro Micro    caterina
+#   Atmel DFU    atmel-dfu
+#   LUFA DFU     lufa-dfu
+#   QMK DFU      qmk-dfu
+#   ATmega32A    bootloadHID
+#   ATmega328P   USBasp
+MCU = atmega32u4
+BOOTLOADER = atmel-dfu
 
 # Build Options - Disable unused features to reduce the size of the binary. 
 # Change yes to no to disable
-#
 # Hardware 
-SPLIT_KEYBOARD = no        # Split common
-BLUETOOTH_ENABLE = no      # Enable Bluetooth with the Adafruit EZ-Key HID
-ENCODER_ENABLE = no        # Enable the use of one or more encoders
-HD44780_ENABLE = no        # Enable support for HD44780 based LCDs
-OLED_DRIVER_ENABLE = no    # Enable the use of OLED displays
+BLUETOOTH_ENABLE = no                # Enable Bluetooth with the Adafruit EZ-Key HID
+CUSTOM_MATRIX = no                   # Declare the use of I/O expander
+ENCODER_ENABLE = no                  # Enable the use of one or more encoders
+HD44780_ENABLE = no                  # Enable support for HD44780 based LCDs
+OLED_DRIVER_ENABLE = no              # Enable the use of OLED displays
+SPLIT_KEYBOARD = no                  # Split, or 1-piece
 # Illumination
-BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality on B7 by default
-RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
-SLEEP_LED_ENABLE = no      # Breathing sleep LED during USB suspend - CAUTION: may conflict with BACKLIGHT_ENABLE
+BACKLIGHT_DRIVER = custom            # Custom driver
+BACKLIGHT_ENABLE = no                # Enable keyboard backlight functionality on B7 by default
+RGBLIGHT_ENABLE = yes                # Enable addressable underglow RGB LEDs - Enabled in info.json
+SLEEP_LED_ENABLE = no                # Breathing sleep LED during USB suspend - CAUTION: may conflict with BACKLIGHT_ENABLE
 # Audio
-EXTRAKEY_ENABLE = yes      # Audio control and System control
-MIDI_ENABLE = no           # MIDI support
-AUDIO_ENABLE = no          # Audio output on port C6
-FAUXCLICKY_ENABLE = no     # Use buzzer to emulate clicky switches
+AUDIO_ENABLE = no                    # Audio output on port C6
+EXTRAKEY_ENABLE = yes                # Audio control and System control
+FAUXCLICKY_ENABLE = no               # Use buzzer to emulate clicky switches
+MIDI_ENABLE = no                     # MIDI support
 # Features
-BOOTMAGIC_ENABLE = no      # Virtual DIP switch configuration
-CONSOLE_ENABLE = no        # Console for debug
-COMMAND_ENABLE = no        # Commands for debug and configuration
-LEADER_ENABLE = no         # Enable the Leader Key feature
-MOUSEKEY_ENABLE = no       # Enable mouse emulation
-NKRO_ENABLE = no           # USB Nkey Rollover - See: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-UNICODE_ENABLE = yes       # Enable unicode support for codes up to 0x7FFF
-LTO_ENABLE = yes           # Link Time Optimization - creates smaller binary
+BOOTMAGIC_ENABLE = no                # Virtual DIP switch configuration
+COMMAND_ENABLE = no                  # Commands for debug and configuration
+CONSOLE_ENABLE = no                  # Console for debug
+DYNAMIC_TAPPING_TERM_ENABLE = yes    # Allow tapping term adjustmet on the fly NOTE: Adjustments are not persistent
+LEADER_ENABLE = yes                  # Enable the Leader Key feature
+LTO_ENABLE = yes                     # Link Time Optimization - creates smaller binary
+MOUSEKEY_ENABLE = no                 # Enable mouse emulation
+NKRO_ENABLE = no                     # USB Nkey Rollover - See: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+UNICODE_ENABLE = yes                 # Enable unicode support for codes up to 0x7FFF
 
-SRC += features/caps_word.c
+SRC += features/caps_word.c          # Include the caps word features folder
+
+# DEFAULT_FOLDER = 
