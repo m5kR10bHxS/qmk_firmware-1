9b9336f056ed6527822ceb280d5150bb19fc6c7c
m5kR10bHxS <dlwhite67@gmail.com>
2022-03-16 21:19:57 -0500

    Remove Space Fn from QWERTY map

--- keyboards/clueboard/66/keymaps/leew1789/keymap.c
+++ keyboards/clueboard/66/keymaps/leew1789/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------+------+------+------+------+------+-------+-------+---------------+-------+
  * |     Shift     |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |  , < |  . > |  /  ? |         Shift         |   Up  |
  * |--------+------+------+------+------+------+------+------+------+------+------+-------+-------+-------+-------+-------+-------+
- * |  Ctrl  |  GUI |  Alt |                     Space Fn(_NAV)                    |  Alt  |  _FN  |  Ctrl | Left  |  Down | Right |
+ * |  Ctrl  |  GUI |  Alt |                     Space                             |  _NAV |  _FN  |  Ctrl | Left  |  Down | Right |
  *  --------+------+------+-------------------------------------------------------+-------+-------+-------+-------+-------+-------+    
  */
 
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,       KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGDN,
   KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN,    KC_QUOT, KC_ENT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,    KC_RSFT,                   KC_UP,
-  KC_LCTL, KC_LGUI, KC_LALT,                   LT(_NAV, KC_SPACE),                 KC_RALT,  MO(_FN),    KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT
+  KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPACE,                           MO(_NAV)  MO(_FN),    KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT
  ),
 
 /* Base Layer: _WORKMAN: (Alternate Base Layer)
@@ -78,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------+------+------+------+------+------+-------+-------+---------------+-------+
  * |               |      |      |      | CCCV |      |Pg Dn |      |      |      |       |                       | Pg Up |
  * |--------+------+------+------+------+------+------+------+------+------+------+-------+-------+-------+-------+-------+-------+
- * |        |      |      |                                                       |       |       |       |       | Pg Dn |       |
+ * |        |Toggle|      |                                                       |       |       |       |       | Pg Dn |       |
  *  --------+------+------+-------------------------------------------------------+-------+-------+-------+-------+-------+-------+    
  */
  
@@ -87,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______, _______, _______, KC_PGUP, KC_UP,   KC_PSCR, _______,  _______, _______, _______,          KC_PGDN,
   KC_CAPS, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RIGHT, KC_INS,  _______, _______,
   _______, _______, _______, _______, CCCV,    _______, KC_PGDN, _______, _______, _______, _______,  _______,                   KC_UP,
-  _______, _______, _______,                            _______,                   _______, _______,  _______,          KC_LEFT, KC_DOWN, KC_RIGHT
+  _______, GUI_TOG, _______,                            _______,                   _______, _______,  _______,          KC_LEFT, KC_DOWN, KC_RIGHT
  ),
 
 /* Function Layer: _FN
