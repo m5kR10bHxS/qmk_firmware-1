030abde8465b0ff2db2bfeedcaf3591a6875cc71
m5kR10bHxS <dlwhite67@gmail.com>
2022-03-16 21:08:50 -0500

    Remove Space Fn from QWERTY map

--- keyboards/alps64/keymaps/leew1789/keymap.c
+++ keyboards/alps64/keymaps/leew1789/keymap.c
@@ -49,16 +49,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |-----------------------------------------------------------------------------------------+
      * | Shift     |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  | RShift | Pg Dn  |
      * |-----------------------------------------------------------------------------------------+
-     * | Ctrl  | LGUI | LAlt  |           Space Fn _NAV             | RAlt |Leader | _Fn | RCtrl |
+     * | Ctrl  | LGUI | LAlt  |           Space                     | _NAV |Leader | _Fn | RCtrl |
      * `-----------------------------------------------------------------------------------------'
      */
 
   [_QWERTY] = LAYOUT_infinity( \
-      KC_ESC,  KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS,  KC_GRV, \
-      KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,          \
-      KC_CAPS, KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,                    \
-      KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_PGDN,                   \
-      KC_LCTL, KC_LGUI, KC_LALT,                 LT(_NAV, KC_SPACE),                                KC_RALT, KC_LEAD, MO(_FN),  KC_RCTL
+      KC_ESC,  KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,     KC_MINS, KC_EQL,  KC_BSLS,  KC_GRV, \
+      KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,     KC_LBRC, KC_RBRC, KC_BSPC,          \
+      KC_CAPS, KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN,  KC_QUOT, KC_ENT,                    \
+      KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, KC_PGDN,                   \
+      KC_LCTL, KC_LGUI, KC_LALT,                 KC_SPACE,                                 MO(_NAV), KC_LEAD, MO(_FN),  KC_RCTL
   ),
 
     /* Alternate Base Layer: _WORKMAN
@@ -93,7 +93,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |-----------------------------------------------------------------------------------------+
      * |           |     |     |     |CCCV |     |Pg Dn|     |     |     |     |        |  Pg Up |
      * |-----------------------------------------------------------------------------------------+
-     * |       |      |       |                                     |      |       |     |       |
+     * |       |Toggle|       |                                     |      |       |     |       |
      * `-----------------------------------------------------------------------------------------'
      */
 
@@ -102,7 +102,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______, _______, _______, _______, _______, _______, _______, KC_PGUP, KC_UP,   KC_PSCR, _______, _______, _______, KC_DEL,           \
       KC_CAPS, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_INS,  _______, _______,                   \
       _______, _______, _______, _______, CCCV,    _______, KC_PGDN, _______, _______, _______, _______, _______, KC_PGUP,                   \
-      _______, _______, _______,                   _______,                                              _______, _______, _______, _______
+      _______, GUI_TOG, _______,                   _______,                                              _______, _______, _______, _______
   ),
 
     /* Function Layer: _FN
